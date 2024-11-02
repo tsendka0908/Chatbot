2d688facbc21b7ea2b4698d6daec3c90010348ab
tsendka0908 <averagetuner06@gmail.com>
2024-11-02 14:41:29 +0800

    Initial commit

---
+++ icons.js
@@ -0,0 +1,23 @@
+export const sendIcon = `
+    <span class="material-symbols-rounded">send</span>
+`;
+export const lightModeIcon = `
+    <span id="mode" class="material-symbols-rounded">
+        light_mode
+    </span>
+`;
+export const deleteIcon = `
+    <span id="delete" class="material-symbols-rounded">
+        delete
+    </span>
+`;
+export const copyIcon = `
+    <span class="material-symbols-rounded copy">
+        content_copy
+    </span>
+`;
+export const volumeUpIcon = `
+    <span class="material-symbols-rounded volume-up">
+        volume_up
+    </span>
+`;
